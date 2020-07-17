acf1e4968f1ade5c58879d2fab9c2e544c618725
Shinanubis <eric_maginot@hotmail.fr>
2020-07-17 09:29:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+/* function for shifted nav person's icon*/
 function shifted() {
+    /* selected all element with person's class */
     let person = document.querySelectorAll(".person");
+    /* iterate for each element */
     for (let i = 0; i < person.length; i++) {
         let pos1 = Math.floor(Math.random() * person.length);
         let pos2 = Math.floor(Math.random() * person.length);
+        /* invert icon */
         person[pos1].before(person[pos2]);
     }
 }
